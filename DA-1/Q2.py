82a4b8891e9f258db31cfcae1877ad294bfac85c
Archit Arora <archit.arora2024@vitstudent.ac.in>
2024-09-03 15:27:13 +0530

    DA-1/Q2

---
+++ DA-1/Q2.py
@@ -0,0 +1,12 @@
+# To find the volume of a cube given the length of a side.
+
+
+# Algorithm
+# 1) Input: Length of Side of the Cube
+# 2) Logic: Volume = (Length)³
+# 3) Output: Volume of Cube in cube units
+
+# Code
+length = int(input("Enter length of side of cube: "))
+vol = length**3
+print("Volume of Cube= ", vol)
