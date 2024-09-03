00bbb76d1c927ce95325b4a84749a17cc58e1cf4
Archit Arora <archit.arora2024@vitstudent.ac.in>
2024-09-03 15:38:12 +0530

    DA-2/Q7

---
+++ DA-2/Q7.py
@@ -0,0 +1,20 @@
+# Write a Python program that performs arithmetic operations (addition, subtraction, multiplication, division, and modulus) on two numbers provided by the user.
+
+
+# Algorithm
+#  Step 1: Input Num1, Num2
+#  Step 2: addition = Num1 + Num2
+#  Step 3: subtraction = Num1 – Num2
+#  Step 4: multiplication = Num1 * Num2
+#  Step 5: division = Num1 / Num2
+#  Step 6: modulus = Num1 % Num2
+#  Step 7: Display addition, subtraction, multiplication, division, and modulus
+
+# Code
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(num1,'+',num2,'=',num1+num2)
+print(num1,'-',num2,'=',num1-num2)
+print(num1,'*',num2,'=',num1*num2)
+print(num1,'/',num2,'=',num1/num2)
+print(num1,'%',num2,'=',num1%num2)
