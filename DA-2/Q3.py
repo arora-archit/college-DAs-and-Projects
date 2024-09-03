ea732cebf5f6dec1ef4d58bd3b58b279fbd85fb3
Archit Arora <archit.arora2024@vitstudent.ac.in>
2024-09-03 15:34:45 +0530

    DA-2/Q3

---
+++ DA-2/Q3.py
@@ -0,0 +1,16 @@
+# You need to calculate the area and perimeter of a rectangle with a length of 10 units and a width of 5 units.
+
+
+# Algorithm
+#  Step 1: Input length and width of rectangle
+#  Step 2: Area = length × width
+#  Step 3: Perimeter = 2 × (length + breadth)
+#  Step 4: Display Area and Perimeter
+
+# Code
+l = float(input("Enter length of rectangle: "))
+w = float(input("Enter length of rectangle: "))
+area = l*w
+perimeter = 2*(l+w)
+print(f"Area = {area}")
+print(f"Perimeter = {perimeter}")
